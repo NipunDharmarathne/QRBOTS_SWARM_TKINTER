16445eb38342ba65a855010c355ea82e49259f0e
NipunDharmarathne <e18077@eng.pdn.ac.lk>
2023-10-24 14:18:00 +0530

    done for getting esp ips to text box

--- ui.py
+++ ui.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from pymavlink import mavutil
+import socket
+import time
 
 '''/// UI ////////////////////////////////////////////////////////////////////////////////////////////////////'''
 master = Tk()
@@ -15,16 +17,29 @@ l2.grid(row = 0, column = 1, sticky = W)
 # Create a Text widget for displaying text
 text = Text(master, wrap="word", width=27, height=5)
 text.grid(row = 1, column=0, columnspan=2)
-label_text = "This is a long text that can be scrolled vertically in a Label widget in Tkinter.\n" * 10
-text.insert("1.0", label_text)
+
+
+def scan():
+    receive_data(source_ip_list)
+
+def start():
+    the_connection = mavutil.mavlink_connection('udp:0.0.0.0:14550')
+
+
+the_connection_1 = mavutil.mavlink_connection('udpout:192.168.123.50:14555')
+def arm():
+    the_connection_1.mav.system_time_send(1, 1)
+
 
 # scan stop buttons
-scan = Button(master, text = "SCAN", bg="springgreen3")
+scan = Button(master, text = "SCAN", bg="springgreen3", command=scan)
+connect = Button(master, text = "CONNECT", bg="springgreen3")
 master.rowconfigure(2, minsize=35)
-scan.grid(row = 2, column=0, columnspan=2)
+scan.grid(row = 2, column=0)
+connect.grid(row = 2, column=1)
 
 # button widget
-b1 = Button(master, text = "ARM ALL", height=3, width=15, bg="springgreen3")
+b1 = Button(master, text = "ARM ALL", height=3, width=15, bg="springgreen3", command=arm)
 b2 = Button(master, text = "DISARM ALL", height=3, width=15, bg="cyan3")
 b3 = Button(master, text = "HOLD ALL", height=3, width=15, bg="olivedrab1")
 b4 = Button(master, text = "RTH ALL", height=3, width=15, bg="chocolate1")
@@ -41,16 +56,51 @@ b6.grid(row = 5, column = 1, sticky = W)
 
 '''/// BACKEND ////////////////////////////////////////////////////////////////////////////////////////////////////'''
 
+def create_udp_socket():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.bind(('0.0.0.0', 14550))
+    return server_socket
+
+def receive_data(source_ip_list):
+    label_text = ""
+    text.delete('1.0', 'end')
+
+    socket = create_udp_socket()
+    start_time = time.time()
+    while time.time() - start_time < 3:
+        data, addr = socket.recvfrom(1024)
+
+        if addr[0] not in source_ip_list:
+            source_ip_list.append(addr[0])
+
+    socket.close()
+    
+    print("ESP IP Addresses:")
+    print(source_ip_list)
+
+    for ip in source_ip_list:
+        label_text += ip + "\n"
+    text.insert("1.0", label_text)
+
+    l2.config(text=len(source_ip_list))
+
+
+source_ip_list = []
+
+
+
+
+
 # Start a connection listening on a UDP port
-the_connection = mavutil.mavlink_connection('udp:0.0.0.0:14550')
-the_connection_1 = mavutil.mavlink_connection('udpout:192.168.123.50:14555')
-the_connection_2 = mavutil.mavlink_connection('udpout:192.168.123.51:14555')
+# the_connection = mavutil.mavlink_connection('udp:0.0.0.0:14550')
+# the_connection_1 = mavutil.mavlink_connection('udpout:192.168.123.50:14555')
+# the_connection_2 = mavutil.mavlink_connection('udpout:192.168.123.51:14555')
 
-def send_func1():
-    the_connection_1.mav.system_time_send(1, 1)
+# def send_func1():
+#     the_connection_1.mav.system_time_send(1, 1)
 
-def send_func2():
-    the_connection_2.mav.param_request_list_send(1, 0)
+# def send_func2():
+#     the_connection_2.mav.param_request_list_send(1, 0)
 
 
 mainloop()
